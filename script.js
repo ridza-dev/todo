396476af97d49f1364a0a0e65dabdce3bb1b45c1
Ahmad Ridza Bin Mohd Rozlan <ahmad.ridza2@gmail.com>
2024-07-18 21:11:37 +1200

    added javascript

--- script.js
+++ script.js
@@ -2,56 +2,96 @@
 
 let todo = JSON.parse(localStorage.getItem("todo")) || [];
 
-const todoInput = document.getElementById("todoInput")
-
+const todoInput = document.getElementById("todoInput");
 
 const todoList = document.getElementById("todoList");
-
 
 const todoCount = document.getElementById("todoCount");
 
 const addButton = document.querySelector(".btn");
 
+const deleteButton = document.getElementById("deleteButton");
 
-const deleteButton = document.getElementById("deleteButton")
-
-
-// Initialize 
-document.addEventListener("DOMContentLoaded",function(){
-    addButton.addEventListener("click", addTask);
-    todoInput.addEventListener('keydown', function(event){
-        if (event.key === 'enter'){
-            event.preventDefault();
-            addTask();
-        }
-    })
-    deleteButton.addEventListener("click", deleteAllTask);
-    displayTasks();
-})
-
-function addTask(){
-    const newTask = todoInput.value.trim();
-    if (newTask !== "" ){
-        todo.push({
-            text: newTask, 
-            disabled: false,
-        });
-        saveToLocalStorage();
-        todoInput.value ="";
-        displayTasks();
+// Initialize
+document.addEventListener("DOMContentLoaded", function () {
+  addButton.addEventListener("click", addTask);
+  todoInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      addTask();
     }
+  });
+  deleteButton.addEventListener("click", deleteAllTask);
+  displayTasks();
+});
 
+function addTask() {
+  const newTask = todoInput.value.trim();
+  if (newTask !== "") {
+    todo.push({ text: newTask, disabled: false });
+    saveToLocalStorage();
+    todoInput.value = "";
+    displayTasks();
+  }
+
+//   console.log("test");
 }
 
-function deleteAllTask(){
-
+function displayTasks() {
+// first the list is emptied 
+  todoList.innerHTML = "";
+  //this is to fetch data from Local Storage and adding them to index.html with html element such as check box, we also see if the element have been disabled or not
+  todo.forEach((item, index) => {
+    const p = document.createElement("p");
+    p.innerHTML = `
+        <div class="todo-container">
+        <input type="checkbox" class="todo-checkbox" id="input-${index}"
+        ${item.disabled ? "checked" : ""}>
+        
+        <p id="todo-${index}" class="${
+      item.disabled ? "disabled" : ""
+    }" onclick="editTask(${index})"> ${item.text}</p>
+        </div>
+        `;
+    p.querySelector(".todo-checkbox").addEventListener("change", () =>
+      toggleTask(index)
+    );
+    todoList.appendChild(p);
+  });
+//   console.log("test");
+  todoCount.textContent = todo.length;
 }
 
+function editTask(index){
+    const todoItem = document.getElementById(`todo-${index}`);
+    const existingText = todo[index].text;
+    const inputElement = document.createElement("input");
+    inputElement.value = existingText;
+    todoItem.replaceWith(inputElement);
+    inputElement.focus();
 
-function displayTasks(){}
-
-
-function  saveToLocalStorage(){
-    localStorage.setItem("todo", JSON.stringify(todo))
+    inputElement.addEventListener("blur", function (){
+        const updatedText = inputElement.value.trim();
+        if (updatedText){
+            todo[index].text = updatedText;
+            saveToLocalStorage();
+        }
+        displayTasks();
+    })
 }
 
+function toggleTask(index) {
+  todo[index].disabled = !todo[index].disabled;
+  saveToLocalStorage();
+  displayTasks();
+}
+
+function deleteAllTask() {
+  todo = [];
+  saveToLocalStorage();
+  displayTasks();
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("todo", JSON.stringify(todo));
+}
